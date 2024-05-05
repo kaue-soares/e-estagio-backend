708f5d58b950a5f4e928665ff2840b49f306de4c
Kauê Soares <kaue222120@gmail.com>
2024-05-05 20:56:46 -0300

    feat: creates api gateway project

---
+++ api-gateway/src/test/java/kauesoares/eestagio/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kauesoares.eestagio.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

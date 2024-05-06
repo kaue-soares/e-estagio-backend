bd7fa2476f52885b3fffefa45945818299e354ce
Kauê Soares <kaue222120@gmail.com>
2024-05-05 21:40:31 -0300

    feat: creates auth service project

---
+++ auth-service/src/test/java/kauesoares/eestagio/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kauesoares.eestagio.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

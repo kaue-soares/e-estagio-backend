f5372c6ebe6929dec7a235b8a3b0302cddd6ead5
Kauê Soares <kaue222120@gmail.com>
2024-05-06 14:00:39 -0300

    feat: creates storage service project

---
+++ storage-service/src/test/java/kauesoares/eestagio/storageservice/StorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kauesoares.eestagio.storageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
